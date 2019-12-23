aad6ef41a2f5f184e02fd540e11942a110788672
Alexandre Abreu <alexandre.abreu@nearsoftsolutions.com>
2019-12-23 09:31:41 +0000

    Updates

---
+++ precache-manifest.eb502bcb91e4a9399e47dd8fde68e647.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdea88941ce73ec3bdb6099a0d781ff3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30a05d22f2633517ee96",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d8eb9d20d45cb75a8140",
+    "url": "/static/css/main.d1fffeb7.chunk.css"
+  },
+  {
+    "revision": "30a05d22f2633517ee96",
+    "url": "/static/js/2.79ebdd15.chunk.js"
+  },
+  {
+    "revision": "1a98684bd549fe20a3eb",
+    "url": "/static/js/3.40fc3e8b.chunk.js"
+  },
+  {
+    "revision": "a5ab5794705b8e08620c",
+    "url": "/static/js/4.6b610828.chunk.js"
+  },
+  {
+    "revision": "d8eb9d20d45cb75a8140",
+    "url": "/static/js/main.647db433.chunk.js"
+  },
+  {
+    "revision": "000e40e4365f3b35ca9e",
+    "url": "/static/js/runtime-main.0eb13923.js"
+  }
+]);
