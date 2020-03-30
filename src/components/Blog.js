6d696d5cf3b265cdff59fc84ff7a903b306d47f5
Alexandre Abreu <alexandre.abreu@nearsoftsolutions.com>
2020-03-30 02:15:19 +0100

    Fix CMS with strapi

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,53 +1,83 @@
 import React, { lazy, Component, Suspense } from 'react';
-import { importMDX } from 'mdx.macro';
-import { Accordion, Card } from 'react-bootstrap';
+import { Accordion, Card, Badge, useAccordionToggle } from 'react-bootstrap';
+import Strapi from 'strapi-sdk-javascript/build/main';
+import ReactMarkdown from "react-markdown";  
+import Moment from "react-moment";
 
-const PostOne = lazy(() => importMDX('../Blog/Post1.mdx'));
-const PostTwo = lazy(() => importMDX('../Blog/laravel-api-models.mdx'));
-const PostThree = lazy(() => importMDX('../Blog/coronavirus.mdx'));
-
+//STRAPI CMS SDK
+const strapi = new Strapi('http://localhost:1337');
 //Styling
 const themeMode = document.querySelector('html').getAttribute('data-theme');
 const modeStyle = (themeMode === 'light') ? '' : '';
 
-class Blog extends Component {
-  render() {
-    return (
-      <div>
-        <Suspense fallback={<div className="text-warning">Loading...</div>}>
-          
-          <Accordion defaultActiveKey="0">
-            <Card className={modeStyle}>
-              <Accordion.Toggle as={Card.Header} eventKey="0">
-                Initial Post - Markdown
-              </Accordion.Toggle>
-              <Accordion.Collapse eventKey="0">
-                <Card.Body>
-                  <PostOne />
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card className={modeStyle}>
-              <Accordion.Toggle as={Card.Header} eventKey="1">
-                Laravel Relationships
-              </Accordion.Toggle>
-              <Accordion.Collapse eventKey="1">
-                <Card.Body><PostTwo /></Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card className={modeStyle}>
-              <Accordion.Toggle as={Card.Header} eventKey="2">
-                COVID-19
-              </Accordion.Toggle>
-              <Accordion.Collapse eventKey="2">
-                <Card.Body><PostThree /></Card.Body>
-              </Accordion.Collapse>
-            </Card>
-          </Accordion>
 
-        </Suspense>
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      posts: []
+    }
+  }
+
+async componentDidMount() {
+   let response = await fetch("http://localhost:1337/posts");
+   if (!response.ok) {
+    return
+  }
+
+  let posts = await response.json()
+  this.setState({ loading: false, posts: posts })
+}
+
+
+render() {
+  if (!this.state.loading) {
+    return (
+      <div className="postsList">
+        <h2 className="ProductList-title">Available Posts <Badge variant="secondary"> {this.state.posts.length} </Badge></h2>
+        <div className="Posts-container">
+          {this.state.posts.map((post, index) => {            
+            function CustomToggle({ children, eventKey }) {
+              const decoratedOnClick = useAccordionToggle(eventKey, () =>
+                console.log('totally custom!'),
+              );
+
+              return (
+                <button
+                  type="button"
+                  style={{ backgroundColor: 'gray', textAlign : 'left' }}
+                  onClick={decoratedOnClick}
+                >
+                  {children}
+                </button>
+              );
+            }
+            return (
+              <Accordion defaultActiveKey={0}>
+                <Card className={modeStyle}>
+                  <CustomToggle as={Card.Header} eventKey={index}>
+                  <b> {post.title} </b>
+                  </CustomToggle>
+                  <Accordion.Collapse eventKey={index}>
+                    <Card.Body>
+                      <ReactMarkdown source={post.data} />
+                      <p className="pt-4">
+                        <Moment format="MMM Do YYYY">{post.published_at}</Moment>
+                      </p>
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
+            );
+          })}
+        </div>
       </div>
     );
   }
+  return (<h2 className="ProductList-title">Waiting for API...</h2>);
+  } 
 }
+
 export default Blog;
