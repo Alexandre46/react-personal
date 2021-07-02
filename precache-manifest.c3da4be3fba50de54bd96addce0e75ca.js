8aa3abb15cfecb7eb2efeb461fb1aecd471d7804
Alexandre Abreu <alexandre.abreu@nearsoftsolutions.com>
2021-07-02 12:22:39 +0100

    Updates

---
+++ precache-manifest.c3da4be3fba50de54bd96addce0e75ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05cdf3f8bed588670bcf111ab9d76ece",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35d85b3b820e89380b2b",
+    "url": "/static/css/2.5d6ae870.chunk.css"
+  },
+  {
+    "revision": "d655111ea70ae9c4a327",
+    "url": "/static/css/main.9ca9baca.chunk.css"
+  },
+  {
+    "revision": "35d85b3b820e89380b2b",
+    "url": "/static/js/2.1d656462.chunk.js"
+  },
+  {
+    "revision": "c428df041ea4c1c023b8c9421aec10a2",
+    "url": "/static/js/2.1d656462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d655111ea70ae9c4a327",
+    "url": "/static/js/main.586f1bd1.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "e0ba39c2d6f299d645278272b8da17e8",
+    "url": "/static/media/cv.e0ba39c2.pdf"
+  },
+  {
+    "revision": "23139ba0d69aaa3756ed382f60703421",
+    "url": "/static/media/nearsoft.23139ba0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c80d7028f7132b9e04123cd12ef150c1.js"
+  "/precache-manifest.c3da4be3fba50de54bd96addce0e75ca.js"
 );
 
 self.addEventListener('message', (event) => {
