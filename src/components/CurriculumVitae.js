c78b482345a120ed8c610554b1a0ad8be3ea2c10
Alexandre Abreu <Alexandreabreu46@hotmail.com>
2021-11-26 00:33:47 +0000

    fix(react-personal): update UI

--- src/components/CurriculumVitae.js
+++ src/components/CurriculumVitae.js
@@ -5,133 +5,149 @@ import CV from "../cv.pdf";
 import CVRedesign from "../cv-redesign.pdf";
 
 const selectors = {
-    resume: "resume",
-    complete: "complete"
-}
+  resume: "resume",
+  complete: "complete",
+};
 
 class CurriculumVitae extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            numPages: null,
-            pageNumber: 1,
-            cvSelected: null,
-            cvFile: null
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      numPages: null,
+      pageNumber: 1,
+      cvSelected: null,
+      cvFile: null,
+    };
+  }
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages: numPages });
+    this.setState({ pageNumber: 1 });
+  };
+
+  changePage = (offset) =>
+    this.setState({ pageNumber: this.state.pageNumber + offset });
+
+  previousPage = () => this.changePage(-1);
+
+  nextPage = () => this.changePage(1);
+
+  showSpecificFile = (string) => {
+    switch (string) {
+      case selectors.resume:
+        this.setState({ cvFile: CVRedesign });
+        break;
+      case selectors.complete:
+        this.setState({ cvFile: CV });
+        break;
+      default:
+        this.setState({ cvFile: CV });
+        break;
     }
+  };
 
-    onDocumentLoadSuccess = ({ numPages }) => {
-        this.setState({numPages: numPages});
-        this.setState({pageNumber: 1});
-    }
+  changeCvSelected = (string) => {
+    this.setState({ cvSelected: string });
+    this.showSpecificFile(string);
+  };
 
-    changePage = (offset) => this.setState({pageNumber: (this.state.pageNumber + offset)});
-
-    previousPage = () => this.changePage(-1);
-  
-    nextPage = () => this.changePage(1);
-
-    showSpecificFile = (string) => {
-          switch(string) {
-              case selectors.resume:
-                this.setState({cvFile: CVRedesign})
-                break;
-              case selectors.complete:
-                this.setState({cvFile: CV})
-                break;
-              default:
-                this.setState({cvFile: CV})
-                break;
-          }
-        
-    }
-
-    changeCvSelected = (string) => {
-        this.setState({cvSelected: string});
-        this.showSpecificFile(string);
-    }
-    
-
-  render () {
+  render() {
     const { t } = this.props;
-    
+
     return (
-        
-        <div className="d-flex justify-content-center py-4 my-4">
-          <div className="container">
-            <div className="row">
-              <div className="col-12 text-center">
-                <h5>{t("cv-select-option")}</h5>
-              </div>
-              <div className="col-12 col-sm-6 text-right">
-                <button 
-                    type="button" 
-                    className={this.state.cvSelected === selectors.resume ? "btn btn-primary active btn-outline-primary" : "btn btn-primary"}
-                    onClick={ () => this.changeCvSelected(selectors.resume)}
-                >
-                  {t("cv-resume")}
-                </button>
-              </div>
-              <div className="col-12 col-sm-6 text-left">
-                <button 
-                    type="button" 
-                    className={this.state.cvSelected === selectors.complete ? "btn btn-secondary active btn-outline-secondary" : "btn btn-secondary"}
-                    onClick={ () => this.changeCvSelected(selectors.complete)}
-                >
-                  {t("cv-complete")}
-                </button>
-              </div>
+      <div className="d-flex justify-content-center py-4 my-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 text-center">
+              <h5>{t("cv-select-option")}</h5>
+            </div>
+            <div className="col-12 col-sm-6 text-right">
+              <button
+                type="button"
+                className={
+                  this.state.cvSelected === selectors.resume
+                    ? "btn btn-primary active btn-outline-primary"
+                    : "btn btn-primary"
+                }
+                onClick={() => this.changeCvSelected(selectors.resume)}
+              >
+                {t("cv-resume")}
+              </button>
+            </div>
+            <div className="col-12 col-sm-6 text-left">
+              <button
+                type="button"
+                className={
+                  this.state.cvSelected === selectors.complete
+                    ? "btn btn-secondary active btn-outline-secondary"
+                    : "btn btn-secondary"
+                }
+                onClick={() => this.changeCvSelected(selectors.complete)}
+              >
+                {t("cv-complete")}
+              </button>
+            </div>
+          </div>
+
+          <div
+            className={
+              this.state.cvSelected
+                ? "row pt-3 px-5 mx-5"
+                : "row pt-3 px-5 mx-5 d-none"
+            }
+          >
+            <div className="col-12 offset-2">
+              <Document
+                file={this.state.cvFile}
+                options={{ workerSrc: "/pdf.worker.js" }}
+                onLoadSuccess={this.onDocumentLoadSuccess}
+              >
+                <Page pageNumber={this.state.pageNumber} />
+              </Document>
             </div>
 
-            <div className={this.state.cvSelected ? "row pt-3 px-5 mx-5" : "row pt-3 px-5 mx-5 d-none"}>
-                <div className="col-12 offset-2">
-                    <Document
-                        file={this.state.cvFile}
-                        options={{ workerSrc: "/pdf.worker.js" }}
-                        onLoadSuccess={this.onDocumentLoadSuccess}
-                    >
-                        <Page pageNumber={this.state.pageNumber} />
-                    </Document>
-                </div>
-    
-                <div className="col-12 offset-2">
-                    <div className="col-6 pt-2 mx-auto">
-                        <p>
-                            {t("cv-page")} {this.state.pageNumber || (this.state.numPages ? 1 : "--")} {t("cv-of")}{" "}
-                            {this.state.numPages || "--"}
-                        </p>
-                        <button
-                            type="button"
-                            disabled={this.state.pageNumber <= 1}
-                            onClick={this.previousPage}
-                        >
-                            {t("cv-previous")}
-                        </button>
-                        <button
-                            type="button"
-                            disabled={this.state.pageNumber >= this.state.numPages}
-                            onClick={this.nextPage}
-                        >
-                            {t("cv-next")}
-                        </button>
-                    </div>
-    
-                    <div className="col-6 pt-2 mx-auto">
-                        <a href={this.state.cvFile} target="_blank" rel="noopener noreferrer" download>
-                            <button className="btn btn-info">
-                            <i className="fas fa-download" />
-                            {t("cv-download-cv")}
-                            </button>
-                        </a>
-                    </div>
-                </div>
+            <div className="col-12 offset-2">
+              <div className="col-6 pt-2 mx-auto">
+                <p>
+                  {t("cv-page")}{" "}
+                  {this.state.pageNumber || (this.state.numPages ? 1 : "--")}{" "}
+                  {t("cv-of")} {this.state.numPages || "--"}
+                </p>
+                <button
+                  type="button"
+                  disabled={this.state.pageNumber <= 1}
+                  onClick={this.previousPage}
+                >
+                  {t("cv-previous")}
+                </button>
+                <button
+                  type="button"
+                  disabled={this.state.pageNumber >= this.state.numPages}
+                  onClick={this.nextPage}
+                >
+                  {t("cv-next")}
+                </button>
+              </div>
+
+              <div className="col-6 pt-2 mx-auto">
+                <a
+                  href={this.state.cvFile}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  download
+                >
+                  <button className="btn btn-info">
+                    <i className="fas fa-download" />
+                    {t("cv-download-cv")}
+                  </button>
+                </a>
+              </div>
             </div>
-            
           </div>
         </div>
-    )
+      </div>
+    );
   }
 }
 
-
-export default withTranslation()(CurriculumVitae)
+export default withTranslation()(CurriculumVitae);
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,40 +1,54 @@
-import React, { Component } from 'react';
-import { bounce } from 'react-animations';
-import Radium, {StyleRoot} from 'radium';
-import { withTranslation } from 'react-i18next';
+import React, { Component } from "react";
+import { bounce } from "react-animations";
+import Radium, { StyleRoot } from "radium";
+import { withTranslation } from "react-i18next";
+import selfie from "../assets/images/selfie.jpg";
 
 const styles = {
-    bounce: {
-      animation: 'x 3s',
-      animationName: Radium.keyframes(bounce, 'bounce')
-    }
+  bounce: {
+    animation: "x 3s",
+    animationName: Radium.keyframes(bounce, "bounce"),
+  },
+};
+
+class Welcome extends Component {
+  render() {
+    const { t } = this.props;
+    const year = new Date().getFullYear();
+
+    return (
+      <StyleRoot>
+        <div
+          className="alert-alert-danger mt-5 text-center"
+          style={styles.bounce}
+        >
+          <h4>
+            {t("introInitial")} <b>Alexandre Abreu</b>
+            <img
+              class="ml-4 my-2 img-fluid img-thumbnail rounded-circle"
+              width={200}
+              height={200}
+              src={selfie}
+              alt="Alexandre abreu selfie"
+            />
+            <br />
+            {t("introEnd")}
+            <br />
+            <span role="img" aria-label="coder">
+              👨🏼‍💻💻 ‍
+            </span>
+          </h4>
+          <div className="col-12 font-italic font-weight-lighter mt-5">
+            {t("introMore", { age: year - 1990 })}
+            <br />
+            <br />
+
+            <h5> {t("introSentence")}</h5>
+          </div>
+        </div>
+      </StyleRoot>
+    );
   }
-
-  class Welcome extends Component {
-    render() {
-        const { t } = this.props;
-        const year = new Date().getFullYear();
-        
-        return(
-            <StyleRoot>
-                <div className="alert-alert-danger mt-5 text-center" style={styles.bounce}>
-                    <h3>{t("introInitial")} <i>Alexandre Abreu</i> {t("introEnd")}
-                    <br/>
-                        <span role="img" aria-label="coder">👨🏼‍💻💻 ‍</span> 
-                    </h3>
-                    <div className="col-12 font-italic font-weight-lighter mt-5">
-                        {t("introMore", { age: year - 1990 })}
-                        <br />
-                        <br />
-
-                        <h5> {t('introSentence')}</h5>
-                    </div>
-                </div>
-            </StyleRoot>
-        );
-    }
-        
 }
-  
 
-  export default withTranslation()(Welcome);
+export default withTranslation()(Welcome);
