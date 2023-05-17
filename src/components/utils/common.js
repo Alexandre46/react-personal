f7716e0ce0de7d71007dc40f945600988b13f03d
Alexandreabreu46 <Alexandreabreu46@hotmail.com>
2023-05-18 00:53:52 +0100

    Updates

--- src/components/utils/common.js
+++
@@ -1,7 +0,0 @@
-const truncate = (str, max = 20) => {
-  const array = str.trim().split(' ');
-  const ellipsis = array.length > max ? '...' : '';
-  return array.slice(0, max).join(' ') + ellipsis;
-};
-
-export default truncate;
