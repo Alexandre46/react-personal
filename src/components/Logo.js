4a4f18b46dc83311bd820bdd53526184d8dc7026
Alexandreabreu46 <Alexandreabreu46@hotmail.com>
2024-02-02 23:52:49 +0000

    fix: cv 2023 en

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Logo = () => {
   return (
     <div className="font-weight-bolder my-auto py-2">
-      <a href="/" style={{ textDecoration: 'none' }}>
+      <a
+        href="/"
+        style={{
+          textDecoration: 'none',
+          color: 'white',
+          fontSize: '40px',
+          WebkitTextStroke: '1px black'
+        }}>
         <span> Alexandre Abreu</span>
       </a>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -51,63 +51,70 @@ const ProjectCard = ({ bgimage, projectname, projectstack, projectinfo, projectr
 
 const MyProjects = () => {
   return (
-    <div className="row" style={styles.root}>
-      <div className="col-12 col-md-6 col-lg-4" style={styles.item}>
-        <div className="btn-info btn-block border">
-          <ProjectCard
-            bgimage="https://res.cloudinary.com/cinforme/image/upload/v1651100683/personal%20website/Screenshot_2022-04-28_at_00.03.53.png"
-            projectname="CINFORME V2"
-            projectstack="NextJS / MongoDB / NodeJS / MaterialUI 5"
-            projectinfo="Complete refactor of news platform CINFORME using Javascript tech for all stack)"
-            projectrepo="https://github.com/Alexandre46/cinforme-version-2"
-          />
+    <>
+      <div className="row">
+        <div className="col-12">
+          <h1 className="text-center">Side Projects</h1>
         </div>
       </div>
-      <div className="col-12 col-md-6 col-lg-4" style={styles.item}>
-        <div className="btn-info btn-block border">
-          <ProjectCard
-            bgimage="https://res.cloudinary.com/cinforme/image/upload/v1639788800/personal%20website/cinforme-img.png"
-            projectname="CINFORME"
-            projectstack="Laravel / Vanilla JS / Bootstrap4"
-            projectinfo="News platform, with an backoffice to insert news and also an news aggregator, using google translator API to store all content in two languages(english and portuguese)"
-            projectrepo="https://github.com/Alexandre46/CINFORME"
-          />
+      <div className="row" style={styles.root}>
+        <div className="col-12 col-md-6 col-lg-4" style={styles.item}>
+          <div className="btn-info btn-block border">
+            <ProjectCard
+              bgimage="https://res.cloudinary.com/cinforme/image/upload/v1651100683/personal%20website/Screenshot_2022-04-28_at_00.03.53.png"
+              projectname="CINFORME V2"
+              projectstack="NextJS / MongoDB / NodeJS / MaterialUI 5"
+              projectinfo="Complete refactor of news platform CINFORME using Javascript tech for all stack)"
+              projectrepo="https://github.com/Alexandre46/cinforme-version-2"
+            />
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-lg-4" style={styles.item}>
+          <div className="btn-info btn-block border">
+            <ProjectCard
+              bgimage="https://res.cloudinary.com/cinforme/image/upload/v1639788800/personal%20website/cinforme-img.png"
+              projectname="CINFORME"
+              projectstack="Laravel / Vanilla JS / Bootstrap4"
+              projectinfo="News platform, with an backoffice to insert news and also an news aggregator, using google translator API to store all content in two languages(english and portuguese)"
+              projectrepo="https://github.com/Alexandre46/CINFORME"
+            />
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-lg-4" style={styles.item}>
+          <div className="btn-info btn-block border">
+            <ProjectCard
+              bgimage="https://res.cloudinary.com/cinforme/image/upload/v1639863220/personal%20website/vue-weather-app.png"
+              projectname="Weather SPA"
+              projectstack="VueJS / DarkSky API / Axios"
+              projectinfo="Vue Weather Single Page Application to show city background image, weekly forecast for geolocation of user"
+              projectrepo="https://github.com/Alexandre46/vue-node-weather"
+            />
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-lg-4" style={styles.item}>
+          <div className="btn-info btn-block border">
+            <ProjectCard
+              bgimage="https://res.cloudinary.com/cinforme/image/upload/v1639864041/personal%20website/website.png"
+              projectname="Portfolio"
+              projectstack="React / React-router / React-boostrap & animations (css)"
+              projectinfo="Personal site/blog just to develop react knowledge "
+              projectrepo="https://github.com/Alexandre46/react-personal"
+            />
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-lg-4" style={styles.item}>
+          <div className="btn-info btn-block border">
+            <ProjectCard
+              bgimage="https://res.cloudinary.com/cinforme/image/upload/v1639863912/personal%20website/hccp.png"
+              projectname="HCCP Mobile App"
+              projectstack="React Native / React Navigation / React Bootstrap"
+              projectinfo="Mobile application to know a bit of Android / iOS apps building a simple app to pick photos / manage on CMS backend and navigate through some screens to see multiple media assets"
+              projectrepo="https://github.com/Alexandre46/react-personal"
+            />
+          </div>
         </div>
       </div>
-      <div className="col-12 col-md-6 col-lg-4" style={styles.item}>
-        <div className="btn-info btn-block border">
-          <ProjectCard
-            bgimage="https://res.cloudinary.com/cinforme/image/upload/v1639863220/personal%20website/vue-weather-app.png"
-            projectname="Weather SPA"
-            projectstack="VueJS / DarkSky API / Axios"
-            projectinfo="Vue Weather Single Page Application to show city background image, weekly forecast for geolocation of user"
-            projectrepo="https://github.com/Alexandre46/vue-node-weather"
-          />
-        </div>
-      </div>
-      <div className="col-12 col-md-6 col-lg-4" style={styles.item}>
-        <div className="btn-info btn-block border">
-          <ProjectCard
-            bgimage="https://res.cloudinary.com/cinforme/image/upload/v1639864041/personal%20website/website.png"
-            projectname="Portfolio"
-            projectstack="React / React-router / React-boostrap & animations (css)"
-            projectinfo="Personal site/blog just to develop react knowledge "
-            projectrepo="https://github.com/Alexandre46/react-personal"
-          />
-        </div>
-      </div>
-      <div className="col-12 col-md-6 col-lg-4" style={styles.item}>
-        <div className="btn-info btn-block border">
-          <ProjectCard
-            bgimage="https://res.cloudinary.com/cinforme/image/upload/v1639863912/personal%20website/hccp.png"
-            projectname="HCCP Mobile App"
-            projectstack="React Native / React Navigation / React Bootstrap"
-            projectinfo="Mobile application to know a bit of Android / iOS apps building a simple app to pick photos / manage on CMS backend and navigate through some screens to see multiple media assets"
-            projectrepo="https://github.com/Alexandre46/react-personal"
-          />
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
